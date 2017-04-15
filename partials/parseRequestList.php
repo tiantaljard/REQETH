f858d3c97ed1ecfeab599e36fb65929b9a5c9c8a
TianTaljard <151172@rgu.ac.uk>
2017-04-15 19:25:17 +0100

    14 April plus: requestList parseRequestList parseUpload requestList again 2

--- partials/parseRequestList.php
+++ partials/parseRequestList.php
@@ -19,17 +19,12 @@ if ($accessgroup == "student") {
     $statement = $db->prepare($sql_query);
     $statement->execute(array(':username' => $username));
     $accessresult = $statement->fetchAll();
-} else if
-($accessgroup == "admin"
-) {
+} else if ($accessgroup == "admin") {
     $sql_query = "select * from users, requests where username=requestor  and (eao1 is null or eao2 is null) and status='submit'; ";
     $statement = $db->prepare($sql_query);
     $statement->execute(array());
     $accessresult = $statement->fetchAll();
-} else if
-($accessgroup == "eao"
-) {
-
+} else if ($accessgroup == "eao"){
     $sql_query = "select * from users, requests 
       where username=requestor and status='assigned' 
       and (eao1=:username or eao2=:username) and not exists (select 1 from comments where comments.username=:username and status='Approve' and comments.request=requests.request); ";
@@ -37,8 +32,4 @@ if ($accessgroup == "student") {
     $statement->execute(array(':username' => $username));
     $accessresult = $statement->fetchAll();
 }
-
-
-
-
 ?>
