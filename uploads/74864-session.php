a48cd27c961cb56a15df11cf1d17528cb2f15ced
TianTaljard <151172@rgu.ac.uk>
2017-04-19 10:47:29 +0100

    Wednesday 10:47

---
+++ uploads/74864-session.php
@@ -0,0 +1,6 @@
+<?php
+// Code below adapted from online course:
+// site:            https://www.udemy.com
+// Course title:    PHP: Complete Login and Registration System with PHP & MYSQL
+// Instructor:      Osayawe Terry Ogbemudia
+session_start();
