89be02177d6f058188bd4aec355fe67f61fd9cbe
tiantaljard <1511762@rgu.ac.uk>
2017-04-22 13:15:18 +0100

    Update parseUpload.php

--- partials/parseUpload.php
+++ partials/parseUpload.php
@@ -40,7 +40,7 @@ if ((isset($_POST['uploadDocBtn']) || isset($_POST['updateRequestBtn'])) && $_FI
         if (move_uploaded_file($file_loc, $folder . $file)) {
             try {
                 //create SQL insert statement
-                $sqlInsert = "INSERT INTO uploads(request,file,name,type,size,fileloc,content) VALUES(:request,:file,:filename,:file_type,:file_size,:file_loc)";
+                $sqlInsert = "INSERT INTO uploads(request,file,name,type,size,fileloc) VALUES(:request,:file,:filename,:file_type,:file_size,:file_loc)";
                 //use PDO prepared to sanitize data
                 $insStatement = $db->prepare($sqlInsert);
                 //add the data into the database
