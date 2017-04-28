bf328608a4eeeec0e9ba691a06da783c2527af2b
TianTaljard <151172@rgu.ac.uk>
2017-04-28 12:58:13 +0100

    During class 12:58

--- api/requests.php
+++ api/requests.php
@@ -85,7 +85,7 @@ function update_request($request)
         );
         header("HTTP/1.0 204 No Content Found");
     }
-    header("HTTP/1.0 204 No Content Found")
+    header("HTTP/1.0 204 No Content Found");
     //header('Content-Type: application/json');
     //echo json_encode($response);
 }
