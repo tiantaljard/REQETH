47828feab251bffb322837c7c8a42e6ff4604e8f
TianTaljard <151172@rgu.ac.uk>
2017-04-29 08:40:12 +0100

    During class 08:31

--- api/consumeapi.php
+++ api/consumeapi.php
@@ -45,8 +45,8 @@ if (isset($_POST['getAPI'])) {
 <form method="post" action="" enctype="multipart/form-data">
     <div style="width: 50%">
         <H2 class='docuheader'>API Test</H2>
-        <H4 class='docuheader'>Root URL: http://reqeth.azurewebsites.net</H4>
-        <H5 class='docuheader'>Available API end points:</H5>
+        <H3 class='docuheader'>Root URL: http://reqeth.azurewebsites.net</H3>
+        <H4 class='docuheader'>Available API end points:</H4>
         <H5 class='docuheader'>/api/requests/</H5>
         <H5 class='docuheader'>/api/requests/:request</H5>
         <H5 class='docuheader'>/api/requestors/</H5>
